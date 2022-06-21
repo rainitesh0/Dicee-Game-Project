bf2b663c6639b5ae8a348eb7356480a63f559572
Nitesh Rai <91134603+rainitesh0@users.noreply.github.com>
2022-06-21 17:02:27 +0530

    Dicee Game code updated.

---
+++ index.js
@@ -0,0 +1,22 @@
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;    // to get random no. b/w 1-6.
+  var randomDiceImage1 = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomImageSource1 = "images/" + randomDiceImage1; //images/dice1.png - images/dice2.png
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src",randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+//if Player1 wins
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else{
+  document.querySelector("h1").innerHTML = "🚩 Draw! 🚩";
+}
